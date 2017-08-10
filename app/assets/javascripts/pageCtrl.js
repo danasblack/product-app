ddc4556fabed20fccd60f137bf0a515751e515a8
danasblack <danasblack@gmail.com>
2017-08-10 14:58:48 -0500

    style changes

--- app/assets/javascripts/pageCtrl.js
+++ app/assets/javascripts/pageCtrl.js
@@ -3,19 +3,49 @@
 	angular.module("app").controller('pageCtrl', ['$scope', function($scope) {
 		$scope.products = [
 		{
-			photo: 'assets/sports-shoes-1.jpg',
+			photo: 'assets/running-shoe-1.jpg',
 			name: 'Nike Air Max',
 			price: 30,
 			type: 'kid'
 		},
 		{
-			photo: 'assets/sports-shoes-2.jpg',
+			photo: 'assets/sports-shoes-1.jpg',
 			name: 'Adidas Classic',
 			price: 45,
 			type: 'woman'
 		},
 		{
+			photo: 'assets/running-shoe-2.jpg',
+			name: 'Nike Air Max',
+			price: 30,
+			type: 'man'
+		},
+		{
+			photo: 'assets/sports-shoes-2.jpg',
+			name: 'Nike Air Max',
+			price: 30,
+			type: 'man'
+		},
+		{
+			photo: 'assets/running-shoe-1.jpg',
+			name: 'Nike Air Max',
+			price: 30,
+			type: 'kid'
+		},
+		{
 			photo: 'assets/sports-shoes-1.jpg',
+			name: 'Adidas Classic',
+			price: 45,
+			type: 'woman'
+		},
+		{
+			photo: 'assets/running-shoe-2.jpg',
+			name: 'Nike Air Max',
+			price: 30,
+			type: 'man'
+		},
+		{
+			photo: 'assets/sports-shoes-2.jpg',
 			name: 'Nike Air Max',
 			price: 30,
 			type: 'man'
